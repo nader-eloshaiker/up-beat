bde3f752af94ecd5866707e8a9eb0c14ac2683b4
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2021-11-08 23:51:45 +1100

    fine tune readings and show logos

--- upbeat.ino
+++ upbeat.ino
@@ -10,6 +10,7 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 128 // OLED display height, in pixels
 #define OLED_RESET -1   //   QT-PY / XIAO
+
 Adafruit_SH1107 display = Adafruit_SH1107(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
@@ -23,9 +24,17 @@ const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
 byte rates[RATE_SIZE]; //Array of heart rates
 byte rateSpot = 0;
 long lastBeat = 0; //Time at which the last beat occurred
+long HEART_RATE_DETECTION = 50000;
 
 float beatsPerMinute;
 int beatAvg;
+int displayBeatAvg = -1;
+
+
+// Bitmaps
+#include "UpBeatLogo.h"
+#include "UpBeatHeartLogo.h"
+
 
 void setup()
 {
@@ -43,13 +52,12 @@ void setup()
   // Initialize sensor
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
-    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    Serial.println("DEBUG: MAX30105 was not found. Please check wiring/power. ");
     while (1);
   }
-  Serial.println("Place your index finger on the sensor with steady pressure.");
 
   particleSensor.setup(); //Configure sensor with default settings
-  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeRed(0x09); //Turn Red LED to low to indicate sensor is running
   particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 }
 
@@ -64,9 +72,12 @@ void loop()
     lastBeat = millis();
 
     beatsPerMinute = 60 / (delta / 1000.0);
+    Serial.print("DEBUG: beatsPerMinute - ");
+    Serial.println(beatsPerMinute);
 
-    if (beatsPerMinute < 255 && beatsPerMinute > 20)
-    {
+
+    // count everything for demo
+    // if (beatsPerMinute > 20 && beatsPerMinute < 255) {
       rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
       rateSpot %= RATE_SIZE; //Wrap variable
 
@@ -75,32 +86,42 @@ void loop()
       for (byte x = 0 ; x < RATE_SIZE ; x++)
         beatAvg += rates[x];
       beatAvg /= RATE_SIZE;
-    }
+    // }
   }
 
+  // quickly detect if it has lost contact with user 
+  if (irValue < HEART_RATE_DETECTION) {
+    beatAvg = 0;
+  }
 
-  if (irValue < 50000) {
-    Serial.print(" No finger?");
-    //obdFill(&obd, 0, 0);
-    //obdWriteStringCustom(&obd, (GFXfont *)&FreeSerif12pt7b, 0, 16, (char *)"No Finger?",1);
-    //obdDumpBuffer(&obd, NULL);
-    display.clearDisplay();
-    display.setTextSize(2);
-    display.setTextColor(SH110X_WHITE);
-    display.setCursor(0, 0);
-    display.println("No Finger?");
-    display.display();
+  // don't bother printing hte same thing on the screen if it hasn't changed
+  if (displayBeatAvg == beatAvg) return;
+
+  if (irValue < HEART_RATE_DETECTION) {
+      display.clearDisplay();
+      display.drawBitmap(19, 0, UpBeatLogo_bits, UpBeatLogo_width, UpBeatLogo_height, SH110X_WHITE);
+      display.display();
+      delay(10);
+      Serial.println("DEBUG: No Heart");
   } else {
-    display.clearDisplay();
-    display.display();
-    Serial.print("IR=");
-    Serial.print(irValue);
-    Serial.print(", BPM=");
-    Serial.print(beatsPerMinute,1);
-    Serial.print(", Avg BPM=");
-    Serial.print(beatAvg);
-
+      display.clearDisplay();
+      display.drawBitmap(19, 0, UpBeatHeartLogo_bits, UpBeatHeartLogo_width, UpBeatHeartLogo_height, SH110X_WHITE);
+      display.setTextColor(SH110X_WHITE);
+      display.setTextSize(3);
+      display.setCursor(0,100);
+      display.print("BPM");             
+      display.setCursor(70,100);   
+      display.print(beatAvg);
+      display.display();
+      delay(10);
+      Serial.print("DEBUG: IR=");
+      Serial.print(irValue);
+      Serial.print(", BPM=");
+      Serial.print(beatsPerMinute,0);
+      Serial.print(", Avg BPM=");
+      Serial.println(beatAvg);
    }
 
-  Serial.println();
+   lastBeat = millis();
+   displayBeatAvg = beatAvg;
 }
